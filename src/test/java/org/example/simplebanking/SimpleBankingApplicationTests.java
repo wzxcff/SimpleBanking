58e2fa422450fe98654c88967941ab864e6a5fd7
wzxcff <Shevasfloyd@gmail.com>
2025-02-07 00:48:14 +0200

    First commit

---
+++ src/test/java/org/example/simplebanking/SimpleBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
